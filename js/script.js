8cf84e65210b0bbff27e0708c0981212cb49d216
Adelia Thomas <116307982+AdeliaTh@users.noreply.github.com>
2026-08-13 20:06:35 -0700

    Add class and innerText toggling for button

--- js/script.js
+++ js/script.js
@@ -8,9 +8,13 @@ var cat = document.querySelector(".cat");
 button.addEventListener("click", function () {
     if (cat.classList.contains("show")) {
         cat.classList.remove("show");
+        button.innerText = "Wait, come back!";
+        button.classList.add("disappear");
         //console.log("Yes!");
     } else {
         cat.classList.add("show");
+        button.innerText = "Shoo, cat!"
+        button.classList.remove("disappear");
         //console.log("No.");
     }
 });
